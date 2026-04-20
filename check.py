849404252381f86bbce2fe56389d69859e57d6c4
newrion-maker <newrion@gmail.com>
2026-04-20 13:48:26 +0900

    Change output to display all top60 items

--- check.py
+++ check.py
@@ -1,5 +1,5 @@
 import json
 data = json.load(open('data.json', encoding='utf-8'))
-print('TOP60 종목:')
-for s in data['top60'][:10]:
+print('TOP60 전체 종목:')
+for s in data['top60']:
     print(s['ticker'], s['name'], s['change'])
